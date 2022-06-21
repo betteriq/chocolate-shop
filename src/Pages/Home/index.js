8569b35c56caaf5ea28143d21afc2d17e4635172
betteriq <betteriq@yahoo.com>
2022-06-21 21:27:53 +0430

    first

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.css"
+
+
+const index = () => {
+    return (
+        <div className='home'>
+            <p>index</p>
+            <h1>hi</h1>
+        </div>
+    )
+}
+
+export default index;
---
+++ src/Pages/Mahsoolat/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+const index = () => {
+    return (
+        <div className='products'>
+            <img src="images/asli1.png" alt="" className='slice_one' />
+            <img src="images/asli2.png" alt="" className='slice_two' />
+        </div>
+    )
+}
+
+export default index
